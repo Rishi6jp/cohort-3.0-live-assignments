fd927758486a145425e393e2bfaf667d27ce108c
Rishi6jp <rainbro187@gmail.com>
2025-04-22 06:56:19 +0530

    updated index.js

--- week-6/basic_auth/index.js
+++ week-6/basic_auth/index.js
@@ -1,74 +1,78 @@
 const express = require('express')
-const app = express();
+const jwt = require('jsonwebtoken')
+const app = express()
 
 app.use(express.json())
 
-const users = [{
-    username: "test123",
-    password: "test123",
-    token: "abc"
-}]
+JWT_SECRET = "abcd"
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const users = []
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
-
+const auth = (req, res, next) => {
+    const token = req.headers.token
+    if(token){
+    const decoded = jwt.verify(token, JWT_SECRET, (err, data) => {
+        if (err) {
+            res.status(401).send({
+                message: "Unauthorized"
+            })
+        } else {
+            req.user = decoded;
+            next();
+        }
+    })
+    }else {
+        res.status(401).send({
+            message: "Unauthorized"
+    })
+}}
 
 app.post('/signup', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = req.body.password; 
 
-    users.push({ username , password});
+    users.push({username, password})
 
     res.json({
-        message: "You are signed in"
+        message: "signed in successfully"
     })
-    console.log(users)
 })
 
 app.post('/signin', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const username = req.body.username
+    const password = req.body.password
 
-    const user = users.find(function(u) {
-        if(u.username === username && u.password === password){
-            return true
-        } else {
-            return false
-        }
-        
-    })
+    
 
-    // res.json({
-    //     user
-    // })
+    let user = users.find(u => u.username === username && u.password === password)
 
     if(user) {
-        const token = generateToken();
-        user.token = token;
+        const token = jwt.sign({username: username}, JWT_SECRET)
+        user.token = token
         res.json({
-            message: token
+            token: token
+        })
+}
+    
+})
+
+app.get('/me',auth,  (req, res) => {
+    const token = req.headers.token
+
+    const decodedInfo = jwt.verify(token, JWT_SECRET)
+    const username = decodedInfo.username;
+
+    let user = users.find(u => u.username === username)
+
+    if(user){
+        res.json({
+            username: user.username, password: user.password
         })
     } else {
-        res.status(403).send({
-            message: "Invalid Username or Password"
+        res.json({
+            message: "Invalid token"
         })
     }
-    // res.json({
-    //     user
-    // })
-    
-    console.log(users);
-
-    
-
 })
 
 app.listen(3000)
