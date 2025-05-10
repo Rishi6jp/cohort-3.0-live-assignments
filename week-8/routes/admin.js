0e476d09145d5aac04afe79e867c5b11965fd83b
Rishi6jp <rainbro187@gmail.com>
2025-05-10 21:35:59 +0530

    added adming and user signup signin routes

--- week-8/routes/admin.js
+++ week-8/routes/admin.js
@@ -1,16 +1,116 @@
 // Admin
 
+const JWT_ADMIN_SECRET = "adf98af6adifh*&8sd"
+const {z} = require('zod')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
 const { Router } = require('express');
-const { AdminModel } = require('../db')
+const { AdminModel, CourseModel } = require('../db')
 
 const adminRouter = Router();
 
-adminRouter.post('/login', (req, res) => {});
 
-adminRouter.post('/signup', (req, res) => {});
+adminRouter.post('/signup', async (req, res) => {
+    const requireBody = z.object({
+        firstName: z.string().max(100).min(3),
+        lastName: z.string().max(100).min(3),
+        email: z.string().email().max(100).min(3),
+        password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/).max(100).min(8)
+        })
+        
+        const parsedDataWithSuccess = requireBody.safeParse(req.body);
+        
+        if(!parsedDataWithSuccess.success){
+            res.json({
+                message: "Incorrect Formate",
+                error: parsedDataWithSuccess.error
+            })
+            return
+        }
+        
+        const {email, password, firstName, lastName } = parsedDataWithSuccess.data
+        let errorThrown = false;
+    
+        try{
+            const hashedPassword = await bcrypt.hash(password, 5);
+            await AdminModel.create({
+                email: email,
+                password: hashedPassword,
+                firstName: firstName,
+                secondName: lastName
+            })  
+        }catch(e){
+            res.json({
+                message: "Can't store into db"
+            })
+            errorThrown = true;
+        }
+    
+        if(!errorThrown){
+            res.json({
+                message: "You are signed In"
+            })
+        }
+    });
+    
+adminRouter.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
 
-adminRouter.post('/course', (req, res) => {});
+    try{
+        const user = await AdminModel.findOne({
+            email: email,
+        })
 
+        if(!user){
+            return res.json({
+                message: "User not found"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+         
+        if(isPasswordCorrect){
+            const token = jwt.sign({
+                id: user._id
+            }, JWT_ADMIN_SECRET)
+            
+            // do a cookie logic
+
+            res.json({
+                token: token
+            })
+        } else {
+            res.status(403).json({
+                message: "Incorrect Credentials"
+            })
+        }
+
+        
+    } catch(e) {
+        console.error("error: "+ e);
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+});
+
+
+adminRouter.post('/course', async (req, res) => {
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price} = req.body;
+
+    const course = await CourseModel.create({
+        name: title, 
+        detail: description,
+        imageUrl: imageUrl,
+        price: price,
+        creatorId: adminId
+    })
+});
+    
 adminRouter.delete('/course', (req, res) => {});
 
 adminRouter.put('/course', (req, res) => {});
--- week-8/routes/user.js
+++ week-8/routes/user.js
@@ -1,12 +1,102 @@
 // Users
 
 const { Router } = require('express');
+const { UserModel } = require('../db');
+const { z } = require('zod')
+const bcrypt = require('bcrypt');
+const { error } = require('console');
+const userRouter = Router();
+const jwt = require('jsonwebtoken');
+const JWT_USER_SECRET = "adf9714qfasdf9q72346234";
 
-const userRouter = Router()
 
-userRouter.post('/login', (req, res) => {});
+userRouter.post('/signup', async (req, res) => {
 
-userRouter.post('/signup', (req, res) => {});
+    const requireBody = z.object({
+        firstName: z.string().max(100).min(3),
+        lastName: z.string().max(100).min(3),
+        email: z.string().email().max(100).min(3),
+        password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/).max(100).min(8)
+    })
+
+    const parsedDataWithSuccess = requireBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message: "Incorrect Formate",
+            error: parsedDataWithSuccess.error
+        })
+        return
+    }
+
+    const {email, password, firstName, lastName } = parsedDataWithSuccess.data
+    let errorThrown = false;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password, 5);
+        await UserModel.create({
+        email: email,
+        password: hashedPassword,
+        firstName: firstName,
+        secondName: lastName
+        })  
+    }catch(e){
+        res.json({
+            message: "Can't store into db"
+        })
+        errorThrown = true;
+    }
+
+    if(!errorThrown){
+        res.json({
+            message: "You are signed In"
+        })
+    }
+    
+});
+
+userRouter.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await UserModel.findOne({
+            email: email,
+        })
+
+        if(!user){
+            return res.json({
+                message: "User not found"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+         
+        if(isPasswordCorrect){
+            const token = jwt.sign({
+                id: user._id
+            }, JWT_USER_SECRET)
+            
+            // do a cookie logic
+
+            res.json({
+                token: token
+            })
+        } else {
+            res.status(403).json({
+                message: "Incorrect Credentials"
+            })
+        }
+
+        
+    } catch(e) {
+        console.error("error: "+ e);
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+    
+});
 
 userRouter.post('/purchases', (req, res) => {});
 
