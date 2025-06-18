b430510510027821cb631f9a49296440302a5e3c
Rishi6jp <rainbro187@gmail.com>
2025-06-18 10:40:04 +0530

    signup & signin route week 15

--- week-15/brainly/src/index.ts
+++ week-15/brainly/src/index.ts
@@ -1,55 +1,96 @@
-import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import { Content, User } from './db';
 import { userMiddleware } from './middleware';
+import z from 'zod'
+import bcrypt from 'bcrypt'
+import express, { Request, Response } from 'express';
 
-require('dotenv').config()
-
-const JWT_PASSWORD = "451904570348"
+import { JWT_PASSWORD } from './config';
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/v1/signup', async (req, res)=> {
+app.post('/api/v1/signup',async (req: Request, res: Response): Promise<void> => {
     //Todo: zod validation, hash the password
-    const username = req.body.username;
-    const password = req.body.password;
+    const requireBody = z.object({
+        username: z.string().max(20).min(3 ),
+        password: z.string()
+            .max(20)
+            .min(8)
+            .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/)
+    })
+
+    const passDataWithSuccess = requireBody.safeParse(req.body);
+    if(!passDataWithSuccess.success){
+        return res.status(400).json({
+            message: "Incorrect Formate",
+            error: passDataWithSuccess.error
+        })
+    }
+    const {username, password} = passDataWithSuccess.data
     try{
+        const hashedPassword = await bcrypt.hash(password, 5);
         await User.create({
         username: username,
-        password: password,
+        password: hashedPassword,
         });
-        res.json({
+        return res.json({
             message : "User signed up"
         });
     }catch(e){
-        res.status(411).json({
+        return res.status(411).json({
             message: "User Already Exist"
         })
     }
 })
 
-app.post('/api/v1/signin', async (req, res)=> {
-    const username = req.body.username;
-    const password = req.body.password;
-    const existingUser = await User.findOne({
-        username,
-        password
-    });
-    if(existingUser){
-        const token = jwt.sign({
-            id: existingUser._id
-        }, JWT_PASSWORD)
-        res.json({
+app.post('/api/v1/signin', async (req: Request, res: Response): Promise<void> => {
+    const singinSchema = z.object({
+        username: z.string().max(20).min(3 ),
+        password: z.string()
+            .max(20)
+            .min(8)
+            .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/)
+    })
+    const parsedSigninDataWithSuccess = singinSchema.safeParse(req.body)
+    if(!parsedSigninDataWithSuccess.success){
+        return res.status(400).json({
+            error: parsedSigninDataWithSuccess.error
+        })
+         
+    }
+
+    const { username, password } = parsedSigninDataWithSuccess.data
+    
+    try{
+        const user = await User.findOne({ username })
+        if(!user){
+            return res.status(403).json({
+                message: "Invalid username or password"
+            })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch){
+            return res.status(403).json({
+                message: "Invalid username or password"
+            })
+        }
+        const token = jwt.sign(
+           { id: user._id, username: user.username },
+           JWT_PASSWORD
+        )
+        return res.json({
+            message: "Signin successful",
             token
         })
-    } else {
-        res.status(403).json({
-            message: "Incorrect Credentials"
+    }catch(err){
+        console.error("Signin error: ", err);
+        return res.status(500).json({
+            message: "Internal server error",
         })
-    }
-})
+    } 
+});
 
 app.post('/api/v1/content', userMiddleware ,async (req, res)=> {
     const link = req.body.link;
